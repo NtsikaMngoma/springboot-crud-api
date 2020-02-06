480c9abcfd2fe814eb12abed79cb777b96b06b6e
Mark Mngoma <trq0348@blake.co.za>
2020-02-06 15:04:47 +0200

    Id DataType Changes

--- src/main/java/com/crud/demo/entities/Product.java
+++ src/main/java/com/crud/demo/entities/Product.java
@@ -1,15 +1,17 @@
 package com.crud.demo.entities;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @Entity
 @Table(name = "product")
 public class Product {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private BigDecimal price;
